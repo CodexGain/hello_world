5d058e7256b764865beae475fbdc9096addecee3
krishna sahu <krishnassm420012@gmail.com>
2023-01-26 17:59:33 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+// console.log(1)
+// console.log(2)
+// setTimeout(() => {
+//     console.log(3);
+// }, 1000);
+// console.log(4)
+// console.log(5)
+let arr = ["mango", "apple", "peach", "banana", "berry"]
+delete arr[3]
+console.log(arr.length)
+console.log(arr);
